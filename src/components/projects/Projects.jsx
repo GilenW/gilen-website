46e7ae76aef834b1a7845777fd2fdaff51917dfb
GfromMacBook <gwuhou@ucsd.edu>
2023-07-15 18:53:53 -0700

    water drop template for project card

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 import img1 from '../../assets/img/proj-img.png'
 import './projects.css'
+import {projects} from './projects-list'
+
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion"
+import { fadeIn, textVariant } from "../../utils/motion";
+import {styles} from "../../utils/styles"
+
+
+
+const ProjectCard = ({index, name, tags, source_link})=>{
+    return (
+    <div className="project-card">
+
+        <p>{name}</p>
+
+        {/* <img src={img1} alt='' className='project-img'></img> */}
+        <ul>
+        {tags.map((tag, index) => (
+          <li key={index}>{tag.name}</li>
+        ))}
+      </ul>
+    </div>
+    )
+}
+
+
 
 
 const Projects = () => {
@@ -8,62 +34,25 @@ const Projects = () => {
 
     <section id='projects'>
         <h2>My Recent Projects</h2>
-        <div className='container project__container'>
-            <article className='project_item'>
-                <h3>This is the project title</h3>
-                <div className='project_item_img'>
-                    <img src={img1} alt=''></img>
 
-                </div>
-                <h4>Skills applied in this project</h4>
-                <div className='project_item_btn'>
-                    <a href='https://github.com' className='btn' target='_blank'>Github</a>
-                    <a href='https://github.com' className='btn' target='_blank'>Live Demo</a>
-                </div>
+        <div className='project-container'>
 
-                {/* Image */}
-                {/* Title */}
-                {/* Tool */}
-                {/* Buttons */}
-            </article>
 
-            <article className='project_item'>
-                <h3>This is the project title</h3>
-                <div className='project_item_img'>
-                    <img src={img1} alt=''></img>
 
-                </div>
-                <h4>Skills applied in this project</h4>
-                <div className='project_item_btn'>
-                    <a href='https://github.com' className='btn' target='_blank'>Github</a>
-                    <a href='https://github.com' className='btn' target='_blank'>Live Demo</a>
-                </div>
 
-                {/* Image */}
-                {/* Title */}
-                {/* Tool */}
-                {/* Buttons */}
-            </article>
+            {projects.map((project, index)=>(
+                <ProjectCard
+                    key={`project-${index}`}
+                    index = {index}
+                    {...project}
+                    />
 
-            <article className='project_item'>
-                <h3>This is the project title</h3>
-                <div className='project_item_img'>
-                    <img src={img1} alt=''></img>
+            ))}
 
-                </div>
-                <h4>Skills applied in this project</h4>
-                <div className='project_item_btn'>
-                    <a href='https://github.com' className='btn' target='_blank'>Github</a>
-                    <a href='https://github.com' className='btn' target='_blank'>Live Demo</a>
-                </div>
-
-                {/* Image */}
-                {/* Title */}
-                {/* Tool */}
-                {/* Buttons */}
-            </article>
 
         </div>
+
+
 
     </section>
   )
@@ -71,3 +60,47 @@ const Projects = () => {
 
 export default Projects
 
+        {/* <div className='container project__container'>
+            <article className='project_item'>
+                <h3>This is the project title</h3>
+
+                <div className='project_item_img'>
+                    <img src={img1} alt=''></img>
+                </div>
+
+                <h4>Skills applied in this project</h4>
+                <div className='project_item_btn'>
+                    <a href='https://github.com' className='btn' target='_blank'>Github</a>
+                    <a href='https://github.com' className='btn' target='_blank'>Live Demo</a>
+                </div>
+            </article>
+
+            <article className='project_item'>
+                <h3>This is the project title</h3>
+                <div className='project_item_img'>
+                    <img src={img1} alt=''></img>
+
+                </div>
+                <h4>Skills applied in this project</h4>
+                <div className='project_item_btn'>
+                    <a href='https://github.com' className='btn' target='_blank'>Github</a>
+                    <a href='https://github.com' className='btn' target='_blank'>Live Demo</a>
+                </div>
+
+            </article>
+
+            <article className='project_item'>
+                <h3>This is the project title</h3>
+                <div className='project_item_img'>
+                    <img src={img1} alt=''></img>
+
+                </div>
+                <h4>Skills applied in this project</h4>
+                <div className='project_item_btn'>
+                    <a href='https://github.com' className='btn' target='_blank'>Github</a>
+                    <a href='https://github.com' className='btn' target='_blank'>Live Demo</a>
+                </div>
+
+            </article>
+
+        </div> */}
---
+++ src/components/projects/projects-list.js
@@ -0,0 +1,75 @@
+
+
+const projects = [
+    {
+        name:"Project one",
+        tags:[
+            {
+                name:"react"
+            },
+            {
+                name:"html/css/js"
+            }
+
+        ],
+        source_link: "github.com"
+
+    },
+    {
+        name:"Project two",
+        tags:[
+            {
+                name:"react"
+            },
+            {
+                name:"html/css/js"
+            }
+
+        ],
+        source_link: "github.com"
+
+    },
+    {
+        name:"Project two",
+        tags:[
+            {
+                name:"react"
+            },
+            {
+                name:"html/css/js"
+            }
+
+        ],
+        source_link: "github.com"
+
+    },
+    {
+        name:"Project two",
+        tags:[
+            {
+                name:"react"
+            },
+            {
+                name:"html/css/js"
+            }
+
+        ],
+        source_link: "github.com"
+
+    },
+    {
+        name:"Project two",
+        tags:[
+            {
+                name:"react"
+            },
+            {
+                name:"html/css/js"
+            }
+
+        ],
+        source_link: "github.com"
+
+    }
+]
+export { projects };
