afd29217a0c0002de8e314c219c0f05fda82214a
GfromMacBook <gwuhou@ucsd.edu>
2023-07-16 22:17:18 -0700

    fixed favicon

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -36,7 +36,8 @@ const About = () => {
           <div className='about-items information'>
             <h1>Gilen Wu Hou </h1>
             <p className='summary'>
-Hi! I'm Gilen, a passionate and driven Junior student at UCSD, majoring in Computer Science. I have a keen interest in software engineering, full-stack web development, machine learning, and data analysis. I am actively seeking opportunities to apply my knowledge and gain valuable experience. If you're looking for a dedicated and collaborative team member, please feel free to reach out! </p>
+              Hi! I'm Gilen, a passionate and driven Junior student at UCSD, majoring in Computer Science. I have a keen interest in software engineering, full-stack web development, machine learning, and data analysis. I am actively seeking opportunities to apply my knowledge and gain valuable experience. If you're looking for a dedicated and collaborative team member, please feel free to reach out!
+            </p>
             <Social/>
           </div>
 
